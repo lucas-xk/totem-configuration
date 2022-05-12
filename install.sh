d4cca610ad2f3f749d122d4b7c8d8cea67d91341
lucas-xk <82394206+lucas-xk@users.noreply.github.com>
2022-05-12 01:56:42 -0300

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+apt update ; apt install chromium openbox lightdm unclutter -y
+
+echo "[SeatDefaults]
+autologin-user=totem
+user-session=openbox" >> /etc/lightdm/lightdm.conf
+
+useradd -m totem && mkdir -p /home/totem/.config/openbox
+
+echo "chromium --no-first-run --disable --disable-translate --disable-infobars --disable-suggestions-service --disable-save-password-bubble --start-maximized --incognito --kiosk https://pesquisasatisfacao.saude.rj.gov.br/unidade/BuscaTipoQuestionario/2298031 &" >> /home/totem/.config/openbox/autostart
+
+chown -R totem:totem /home/totem/.config/ && chmod -R 700 /home/totem/.config/
+
+reboot

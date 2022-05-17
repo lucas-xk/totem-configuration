a1639fb18cb826a478e79e5e7bff165aae2618b5
lucas-xk <82394206+lucas-xk@users.noreply.github.com>
2022-05-17 16:05:15 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,7 +8,8 @@ user-session=openbox" >> /etc/lightdm/lightdm.conf
 
 useradd -m totem && mkdir -p /home/totem/.config/openbox
 
-echo "unclutter -idle 0 &
+echo "xset -dpms s off &
+unclutter -idle 0 &
 chromium --no-first-run --disable --disable-translate --disable-infobars --disable-suggestions-service --disable-save-password-bubble --start-maximized --incognito --kiosk https://pesquisasatisfacao.saude.rj.gov.br/unidade/BuscaTipoQuestionario/2298031 &" >> /home/totem/.config/openbox/autostart
 
 chown -R totem:totem /home/totem/.config/ && chmod -R 700 /home/totem/.config/
